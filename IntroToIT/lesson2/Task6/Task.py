830ef15573dee55f75ba615a9fed5cd58b939fe1
Fellkings <fellkings51@gmail.com>
2024-04-03 18:25:06 +0300

    refactor: translation of the code into English

--- IntroToIT/lesson2/Task6/Task.py
+++ IntroToIT/lesson2/Task6/Task.py
@@ -1,7 +1,7 @@
 #INTRO TO IT 2nd COURSE
 # Задача 6: Гласные в высоте!
 # Посчитай количество гласных букв в строке.
-def подсчет_гласных(строка):
-    return sum(1 for символ in строка if символ.lower() in "аеёиоуыэюя")
-строка = "Привет, мир!"
-print(f"В '{строка}' {подсчет_гласных(строка)} гласных")
+def counting_vowels(string):
+    return sum(1 for symbol in string if symbol.lower() in "аеёиоуыэюя")
+string = "Привет, мир!"
+print(f"В '{string}' {counting_vowels(string)} гласных")
