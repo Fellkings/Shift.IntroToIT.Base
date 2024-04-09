bba591e056da5a841b751a30e109ffc28c557a70
Fellkings <fellkings51@gmail.com>
2024-04-09 04:22:34 +0300

    fix: code correction

--- IntroToIT/lesson3/Task10/Task.py
+++ IntroToIT/lesson3/Task10/Task.py
@@ -1,6 +1,7 @@
 #INTRO TO IT 2nd COURSE
-s = input("Введите строку: ").replace(' ', '').lower()
-if s = s[::-1]:
+s = input("Введите строку: ")
+reversed_s = s[::-1]
+if s == reversed_s:
     print("Строка является палиндромом")
 else:
     print("Строка не является палиндромом")
